d290e8669c748508a1c19fac6c11d3f386b28b45
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-03-19 11:01:11 -0700

    completed asignment

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Write your solution in this file!
+// let employee;
+
+// beforeEach(() => {
+//     employee = {
+//       name: "Alice",
+//       streetAddress: "1234 Elm Street",
+//     };
+//   });
+
+// function () {
+//      let employeee = {
+//         name: "Sam",
+//         streetAdress: "1234 Elm Street"
+//     }
+    
+// }
+
+const employee =  {
+    firstName: 'Emmanuel',
+    lastName: 'Kipsang',
+    role: 'Software Engineer',
+    payPerHour: 35,
+    timeInHours: 160,
+    hiredDate: new Date('2022-08-01'),
+    email: 'emmanuel@example.com',
+    phoneNumber: '555-555-5555'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  
+  
+  
+  
+  
